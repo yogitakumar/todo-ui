90f13353b45071caecbf8ea4f4ae812304f622a4
yogitakumar <helloyogita@gmail.com>
2021-04-05 21:26:40 +0100

    adding extra task

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
     { id: "001", text: "Buy cat food", completed: false, date: "2019-09-18" },
     { id: "002", text: "Wash dishes", completed: true, date: "2019-09-24" },
     { id: "003", text: "Help to kids to complete their art&craft", completed: false, date: "2019-09-27" },
-    { id: "004", text: "Buy a milk", completed: true, date: "2019-09-20" }
+    { id: "004", text: "Buy a milk", completed: true, date: "2019-09-20" },
+    { id: "007", text: "Mending broken table", completed: false, date: "2019-09-22" }
   ])
   const deleteTask = id => {
     const updatedTasks = tasks.filter(task => task.id !== id)
