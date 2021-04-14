3bbdc5b00ce662beb091593ecc3f79b5f4d5fe9b
yogitakumar <helloyogita@gmail.com>
2021-04-14 23:38:05 +0100

    testing

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,14 +1,15 @@
-import './Task.css';
+import "./Task.css";
 
 function Task({ complete, text, id, deleteTask }) {
-
-    return (
-        <li className={`task ${complete ? 'task--complete' : ''}`}>
-            <p className="task__text">{text}</p>
-            { complete === false && <button className="button">Complete</button>}
-            <button onClick={() => deleteTask(id)} className="button">Delete</button>
-        </li>
-    );
+  return (
+    <li className={`task ${complete ? "task--complete" : ""}`}>
+      <p className="task__text">{text}</p>
+      {complete === false && <button className="button">Complete</button>}
+      <button onClick={() => deleteTask(id)} className="button">
+        Delete
+      </button>
+    </li>
+  );
 }
 
 export default Task;
